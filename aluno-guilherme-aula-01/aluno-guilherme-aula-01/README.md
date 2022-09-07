c727f84992ebce4eee064581317a4cefdb5483f1
shxtz <112507135+shxtz@users.noreply.github.com>
2022-09-07 12:39:36 -0300

    Add files via upload

---
+++ aluno-guilherme-aula-01/aluno-guilherme-aula-01/README.md
@@ -0,0 +1,4 @@
+# Aluno: Guilherme-Aula-01
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Dos-Santos-Santana/pen/VwxYVgp](https://codepen.io/Dos-Santos-Santana/pen/VwxYVgp).
+
---
+++ aluno-guilherme-aula-01/aluno-guilherme-aula-01/dist/script.js
@@ -0,0 +1,17 @@
+var nome = "Guilherme"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 8
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 5
+var somaNotas = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre)
+var notaFinal = 26.0 / 4
+var notaFixada = notaFinal.toFixed(1)
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+alert(nome)
+alert(notaFixada)
+
+//comentários
+
+//Revisão
+//varíaveis, strings, console.log, toFixed, operações matemáicas, concatenação
